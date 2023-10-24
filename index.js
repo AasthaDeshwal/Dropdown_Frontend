6ea4d992e6acf261a4b618be4202549672a390d6
AasthaDeshwal25 <145780627+AasthaDeshwal25@users.noreply.github.com>
2023-10-24 12:40:24 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const subMenuIcons = document.querySelectorAll(".sub-menu-icon");
+const subMenus = document.querySelectorAll(".sub-menu");
+const mobileMenuIcon = document.getElementById("mobile-menu-icon");
+const mobileMenuCloseIcon = document.getElementById("mobile-menu-close-icon");
+const headerNav = document.getElementById("header-nav");
+const images = document.querySelectorAll(".content-client-img");
+ 
+mobileMenuIcon.addEventListener("click", () => {
+  console.log("click mobile icon ");
+  mobileMenuIcon.classList.remove("show");
+  mobileMenuIcon.classList.add("hidden");
+  mobileMenuCloseIcon.classList.remove("hidden");
+  mobileMenuCloseIcon.classList.add("show");
+  headerNav.style.display = "inherit";
+})
+ 
+mobileMenuCloseIcon.addEventListener("click", () => {
+  console.log("click on mobile close menu icon ");
+  mobileMenuCloseIcon.classList.remove("show");
+  mobileMenuCloseIcon.classList.add("hidden");
+  mobileMenuIcon.classList.remove("hidden");
+  mobileMenuIcon.classList.add("show");
+  headerNav.style.display = "none";
+})
+
+let prevIndex = null;
+
+subMenuIcons.forEach((icon,index) => {
+  icon.addEventListener("click", () => {
+    if(prevIndex !== null){
+      subMenuIcons[prevIndex].src = "images/icon-arrow-down.svg";
+      subMenus[prevIndex].classList.add("hidden");
+      subMenus[prevIndex].classList.remove("show");
+    }
+
+    if(index === prevIndex){
+      prevIndex = null;
+    }else{
+      icon.src="images/icon-arrow-up.svg";
+      subMenus[index].classList.remove("hidden");
+      subMenus[index].classList.add("show");
+      prevIndex= index;
+    }
+  })
+})
+
+
+ 
+images.forEach((image, index) => {
+  const delay = index * 1000;
+
+  setTimeout(() => {
+    image.style.opacity = 1;
+    image.style.transform = "translateX(0)";
+  },delay)
+
+})
